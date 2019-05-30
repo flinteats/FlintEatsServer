a53e768bbed2e1ea1208a96c922f42d38fe2cc9a
Aiden Hoffman <hoffman.aiden@gmail.com>
2019-05-30 15:35:08 -0400

    FunChanges

--- src/main/java/com/etshost/msu/entity/Recipe.java
+++ src/main/java/com/etshost/msu/entity/Recipe.java
@@ -7,9 +7,19 @@ import org.springframework.roo.addon.json.RooJson;
 import org.springframework.roo.addon.tostring.RooToString;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 /**
  * Represents a recipe for a culinary dish.
  */
+@Analyzer(impl = org.apache.lucene.analysis.standard.StandardAnalyzer.class)
 @Audited
 @javax.persistence.Entity
 @Configurable
@@ -20,8 +30,157 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class Recipe extends UGC {
     
-    private int servings;
+    @Field(index=Index.YES, analyze=Analyze.YES, store=Store.NO)	
+    private String title;
 
-    private String directions;
+    @JSON(include = false)
+	private byte[] image;
+    private String ingredientList;
+    private String steps;
+
+    @JsonCreator
+	public static Recipe factory(
+			@JsonProperty("title") String title,
+			@JsonProperty("ingredientList") String ingredientList,
+            @JsonProperty("image") String image64,
+            @JsonProperty("steps") String steps,
+            @JsonProperty("tags") Set<Tag> tags
+			) {
+		Recipe recipe = new Recipe();
+        Logger logger = LoggerFactory.getLogger(Recipe.class);
+		logger.debug("Recipe factory. tags: {}", tags);
+		recipe.setImageBase64(image64);
+		recipe.setTitle(title);
+		recipe.setTags(tags);
+        recipe.setIngredientList(ingredientList);
+        recipe.setSteps(steps);
+		return recipe;
+	}
+    @JSON(include = false)
+    public byte[] getImage() {
+    	return this.image;
+    }
+    
+    @JSON(include = false)
+    public String getImageBase64() {
+    	if (this.image == null) {
+    		return null;
+    	}
+    	String image64 = Base64.getEncoder().encodeToString(this.image);
+        return image64;
+    }
+    @JSON(name = "image64")
+	public String getImageBase64Scaled() {
+    	if (this.image == null) {
+    		return null;
+    	}
+		final InputStream in = new ByteArrayInputStream(this.image);
+		BufferedImage image;
+		BufferedImage imageScaled;
+
+		// read in the image
+		try {
+			image = ImageIO.read(in);
+		} catch (final IOException e) {
+			// TODO Auto-generated catch block. Come up with better return.
+			e.printStackTrace();
+			return null;
+		}
+		if (image == null) {
+			return null;
+		}
+		
+		imageScaled = Scalr.resize(image, 360);
+
+		// write the image
+		final ByteArrayOutputStream output = new ByteArrayOutputStream();
+		try {
+			ImageIO.write(imageScaled, "png", output);
+		} catch (final IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			// Come up with better return
+			return null;
+		}
+    	String image64 = Base64.getEncoder().encodeToString(output.toByteArray());
+		return image64;
+	}
+	
+    public void setImageBase64(String image64) {
+    	try {
+    		byte[] image = Base64.getMimeDecoder().decode(image64);
+            this.setImage(image);
+    	} catch (Exception e) {
+    		this.logger.error(e.toString());
+    	}
+    }
+    
+    public String toJson() {
+        return new JSONSerializer()
+        		.exclude("logger").serialize(this);
+    }
+    
+    public static String toJsonArrayRecipe(Collection<Recipe> collection) {
+        return new JSONSerializer()
+		        .exclude("*.class", "*.logger", )
+    }
+
+    // pulled from Roo file due to bug [ROO-3570]
+    public static List<Recipe> findRecipeEntries(int firstResult, int maxResults,
+    		String sortFieldName, String sortOrder) {
+    	if (sortFieldName == null || sortOrder == null) {
+    		return Recipe.findRecipeEntries(firstResult, maxResults);
+    	}
+        String jpaQuery = "SELECT o FROM Recipe o";
+        if (fieldNames4OrderClauseFilter.contains(sortFieldName)
+        		|| Entity.fieldNames4OrderClauseFilter.contains(sortFieldName)) {
+            jpaQuery = jpaQuery + " ORDER BY " + sortFieldName;
+            if ("ASC".equalsIgnoreCase(sortOrder) || "DESC".equalsIgnoreCase(sortOrder)) {
+                jpaQuery = jpaQuery + " " + sortOrder;
+            }
+        }
+        if (maxResults < 0) {
+            return entityManager().createQuery(jpaQuery, Recipe.class)
+            		.setFirstResult(firstResult).getResultList();
+        }
+        return entityManager().createQuery(jpaQuery, Recipe.class)
+        		.setFirstResult(firstResult).setMaxResults(maxResults).getResultList();
+    }
+    
+    @SuppressWarnings("unchecked")
+	public static List<Recipe> search(String q) {
+    	Logger logger = LoggerFactory.getLogger(Recipe.class);
+		EntityManager em = Entity.entityManager();
+		FullTextEntityManager fullTextEntityManager = Search.getFullTextEntityManager(em);
+
+		logger.debug("searching Recipe for: {}", q);
+
+		QueryBuilder qb = fullTextEntityManager
+				.getSearchFactory()
+				.buildQueryBuilder()
+				.forEntity(Recipe.class)
+				.get();
+		org.apache.lucene.search.Query luceneQuery = qb
+			    .keyword()
+			    .fuzzy()
+			    .onFields("title", "price", "steps","ingredientlist")
+				.matching(q)
+				.createQuery();
+		
+//		logger.debug("luceneQuery: {}", luceneQuery.toString());
+
+
+		// wrap Lucene query in a javax.persistence.Query
+		javax.persistence.Query jpaQuery =
+		    fullTextEntityManager.createFullTextQuery(luceneQuery, Recipe.class);
+//		logger.debug("jpaQuery: {}", jpaQuery.toString());
+		
+
+		// execute search
+		logger.debug("results size: {}", jpaQuery.getResultList().size());
+		List<?> result = jpaQuery.getResultList();
+		return (List<Recipe>)result;
+    }
+		
     
 }
--- src/main/java/com/etshost/msu/web/RecipeController.java
+++ src/main/java/com/etshost/msu/web/RecipeController.java
@@ -8,4 +8,98 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/ugc/recipes")
 @RestController
 public class RecipeController {
+    protected final Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	/**
+	 * Creates a new Recipe from the JSON description
+	 * @param recipe	Recipe to create
+	 * @return		ID of created Recipe
+	 */
+	@RequestMapping(value = "/create", method = RequestMethod.POST, produces = "application/json")
+	public String create(@RequestBody Recipe recipe) {
+		this.logger.debug("landed at /ugc/recipes/create");
+		
+		recipe.setUsr(User.getLoggedInUser());
+		/*
+		JsonArray errors = new JsonArray();
+		if (errors.size() > 0) {
+			return errors.toString();
+		}
+		*/
+
+		// persist and return id
+		recipe.persist();
+		return recipe.getId().toString();
+	}
+	
+	/**
+	 * Returns JSON list of Recipes
+	 * @param start			index of first item
+	 * @param length		number of items to return
+	 * @param orderField	field to order results by
+	 * @param orderDir		order direction (ASC or DESC)
+	 * @return				JSON array of results
+	 */
+	@RequestMapping(value = "/list", method = RequestMethod.GET, produces = "application/json")
+	public String list(
+			@RequestParam(name = "start", defaultValue = "0") int start,
+			@RequestParam(name = "length", defaultValue = "-1") int length,
+			@RequestParam(name = "orderField", required = false) String orderField,
+			@RequestParam(name = "orderDir", defaultValue = "ASC") String orderDir) {
+				
+		List<Recipe> results = Recipe.findRecipeEntries(start, length, orderField, orderDir);
+		return Recipe.toJsonArray(results);
+	}
+	
+	
+	
+	/**
+	 * Updates the Recipe having the given ID
+	 * @param id	ID of Recipe to update
+	 * @param recipe	updated Recipe
+	 * @return		ID of updated Recipe
+	 */
+	@PreAuthorize("hasAuthority('admin')")
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT, produces = "application/json")
+	public String update(@PathVariable("id") long id, @RequestBody Recipe recipe) {
+		if (recipe.getId() != id) {
+			return "ID error";
+		}
+		
+		/*
+		JsonArray errors = new JsonArray();
+		if (errors.size() > 0) {
+			return errors.toString();
+		}
+		*/
+        final Recipe oldRecipe = Recipe.findRecipe(recipe.getId());
+        recipe.setVersion(oldRecipe.getVersion());
+        recipe.setCreated(oldRecipe.getCreated());
+        recipe.setStatus(oldRecipe.getStatus());
+		// merge and return id
+		recipe.merge();
+		return recipe.getId().toString();
+	}
+	
+	/**
+	 * Returns JSON representation of Recipe with the given ID
+	 * @param id	ID of Recipe to view 
+	 * @return		JSON of Recipe
+	 */
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = "application/json")
+	public String view(@PathVariable("id") long id) {
+		Recipe recipe = Recipe.findRecipe(id);
+		if (recipe == null) {
+			return "0";
+		}
+		User user = User.getLoggedInUser();
+		new Viewing(user, recipe).persist();
+		recipe.merge(); // update updated time
+		return recipe.toJson();
+	}
+	
+	@RequestMapping(value = "/search", method = RequestMethod.GET, produces = "application/json")
+	public String search(@RequestParam(name = "q", required = true) String q) {
+		return Recipe.toJsonArrayRecipe(Recipe.search(q));
+	}
 }

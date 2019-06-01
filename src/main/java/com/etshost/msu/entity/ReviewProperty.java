2253e2a9edf118f898c0dcfed1e8e3ecea8452d2
Aiden Hoffman <hoffman.aiden@gmail.com>
2019-06-01 15:16:23 -0400

    Revert "yeetz"
    This reverts commit 3de3c9a6d11a2e3dea0b61664bcfc0b23b6a81cd.

--- src/main/java/com/etshost/msu/entity/ReviewProperty.java
+++ src/main/java/com/etshost/msu/entity/ReviewProperty.java
@@ -97,7 +97,7 @@ public class ReviewProperty extends UGC {
 			// @JsonProperty("propertyType") long startEpoch,
 			// @JsonProperty("value") long endEpoch,
 			@JsonProperty("id") long id,
-			@JsonProperty("market") Market market,
+			@JsonProperty("review_id") Market review_id,
 			@JsonProperty("cleanrating") int cleanrating,
 			@JsonProperty("selectionrating") int selectionrating,
 			@JsonProperty("accessrating") int accessrating,
